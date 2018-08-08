367c6f2e0fc6925fdbc9664587ceab95bcb4dc31
Brian Donovan <donovan@squareup.com>
2018-08-08 10:55:28 -0700

    Add support for sharing files from Finder.

--- sharing.lua
+++ sharing.lua
@@ -7,12 +7,16 @@ function shareFromFrontmostApplication()
 	local appID = app:bundleID()
 	local appName = app:name()
 	local applescript
+	local isFileURL = false
 
 	log.f("Attempting to share from %s (%s)", appName, appID)
 	if appID == "com.google.Chrome" then
 		applescript = string.format("tell application \"%s\" to get URL of active tab of front window as string", appName)
 	elseif appID == "com.apple.Safari" then
 		applescript = string.format("tell application \"%s\" to return URL of front document as string", appName)
+	elseif appID == "com.apple.finder" then
+		applescript = string.format("tell application \"%s\" to get URL of (selection as alias)", appName)
+		isFileURL = true
   else
     hs.notify.new({title="Unable to Share", informativeText=string.format("I don't know how to share from \"%s\"", appName)}):send()
 		return
@@ -22,7 +26,7 @@ function shareFromFrontmostApplication()
 
 	if success then
 		local shareSheet = hs.sharing.newShare("com.apple.share.AirDrop.send")
-		local url = hs.sharing.URL(output)
+		local url = hs.sharing.URL(output, isFileURL)
 
 		shareSheet:shareItems({ url })
 	end
