79f6b03fc687a04865dc1b4e0120b28b5f63eb82
Brian Donovan <donovan@squareup.com>
2018-10-15 15:14:29 -0700

    Send ESC when tapping Caps Lock.

--- init.lua
+++ init.lua
@@ -91,6 +91,42 @@ hs.hotkey.bind({"cmd", "ctrl"}, "E", function()
   expose:toggleShow()
 end)
 
+
+-- Sends "escape" if "caps lock" is tapped, and no other keys are pressed.
+-- https://stackoverflow.com/questions/41094098/hammerspoon-remap-control-key-sends-esc-when-pressed-alone-send-control-when-p
+
+local send_escape = false
+local last_mods = {}
+local control_key_timer = hs.timer.delayed.new(0.1, function()
+  send_escape = false
+end)
+
+hs.eventtap.new({hs.eventtap.event.types.flagsChanged}, function(evt)
+  local new_mods = evt:getFlags()
+  if last_mods["ctrl"] == new_mods["ctrl"] then
+    return false
+  end
+  if not last_mods["ctrl"] then
+    last_mods = new_mods
+    send_escape = true
+    control_key_timer:start()
+  else
+    if send_escape then
+      hs.eventtap.keyStroke({}, "escape")
+    end
+    last_mods = new_mods
+    control_key_timer:stop()
+  end
+  return false
+end):start()
+
+
+hs.eventtap.new({hs.eventtap.event.types.keyDown}, function(evt)
+  send_escape = false
+  return false
+end):start()
+
+
 -- Caffeine
 
 local caffeine = hs.menubar.new()
